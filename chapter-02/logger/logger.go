9e518d1cf73f250155e2ad6596c7cd34e84a0b4f
Pascal <floppyzedolfin@gmail.com>
2022-03-25 12:36:43 +0000

    Testing the Info level

--- chapter-02/logger/logger.go
+++ chapter-02/logger/logger.go
@@ -31,20 +31,24 @@ func (l Logger) WithOutput(output io.Writer) Logger {
 // Debug formats and prints a message if the log level is debug or higher
 func (l Logger) Debug(format string, args ...any) {
 	if l.level <= LevelDebug {
-		_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+		l.log(format, args)
 	}
 }
 
 // Info formats and prints a message if the log level is info or higher
 func (l Logger) Info(format string, args ...any) {
 	if l.level <= LevelInfo {
-		_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+		l.log(format, args)
 	}
 }
 
 // Error formats and prints a message if the log level is error or higher
 func (l Logger) Error(format string, args ...any) {
 	if l.level <= LevelError {
-		_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+		l.log(format, args)
 	}
+}
+
+func (l Logger) log(format string, args []any) {
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
