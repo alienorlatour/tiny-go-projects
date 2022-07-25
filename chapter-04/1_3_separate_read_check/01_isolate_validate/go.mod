ba44afeb4f84141183b69bf2630f21ab361158d6
Donia Chaiehloudj <donia.cld@gmail.com>
2022-07-25 15:53:54 +0200

    ch04: 1.3.1 Add isolate validation subsection

---
+++ chapter-04/1_3_separate_read_check/01_isolate_validate/go.mod
@@ -0,0 +1,3 @@
+module github.com/ablqk/tiny-go-projects/chapter-04/1_3_separate_read_check/01_isolate_validate
+
+go 1.18
--- chapter-04/1_3_separate_read_check/01_isolate_validate/main.go
+++ chapter-04/1_3_separate_read_check/01_isolate_validate/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/ablqk/tiny-go-projects/chapter-04/1_3_separate_read_check/gordle"
+	"github.com/ablqk/tiny-go-projects/chapter-04/1_3_separate_read_check/01_isolate_validate/gordle"
 )
 
 func main() {
