620c8597cc7d420a88927ab93f49bbe367091dba
Aliénor Latour <alienor.latour@hellofresh.com>
2022-09-08 13:12:32 +0200

    Parsing flags

---
+++ chapter-05/final/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+// Usage: calconvert -from gregorian -to hijri -date “5 February 2022”
+
+func main() {
+	from := flag.String("from", "gregorian", "source calendar")
+	to := flag.String("to", "gregorian", "target calendar")
+	date := flag.String("date", time.Now().Format("2006-01-02"), "date to convert")
+
+	flag.Parse()
+
+	fmt.Println(*from, *to, *date)
+}
