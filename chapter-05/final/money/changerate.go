4114df7e7076830b2511dc7ee0396d559b9e680c
Donia Chaiehloudj <donia.cld@gmail.com>
2022-09-15 21:40:41 +0200

    ch05: apply change rate

--- chapter-05/final/money/changerate.go
+++ chapter-05/final/money/changerate.go
@@ -4,11 +4,11 @@ const (
 	errUnknownChangeRate = moneyError("no change rate known between currencies")
 )
 
-// changeRate is a float64, because the precision of a float32 is limited at 6 digits after the first
-// significant one. This means we introduce a 1 euro error when we convert numbers above 10^7 euros.
-type changeRate float64
+// changeRate is a float32, because the precision of an official change rate is 5 significant digits.
+type changeRate float32
 
 // fetchChangeRate is in charge of retrieving the change rate between two currencies.
 func fetchChangeRate(from, to string) (changeRate, error) {
+
 	return 1.5168, nil
 }
