db2c396ddf456ca65239efaf605ca45a135a693f
Donia Chaiehloudj <donia.cld@gmail.com>
2022-09-29 13:59:14 +0200

    ch05: Add get currency tests

--- chapter-05/final/money/currency.go
+++ chapter-05/final/money/currency.go
@@ -5,3 +5,22 @@ type currency struct {
 	code      string
 	precision int
 }
+
+// errUnknownCurrency is returned when a currency is unsupported.
+const errUnknownCurrency = moneyError("unknown currency")
+
+// currencies defines the supported currencies.
+var currencies = map[string]currency{
+	"EUR": {code: "EUR", precision: 2},
+	"USD": {code: "USD", precision: 2},
+	"ETH": {code: "ETH", precision: 9},
+}
+
+// getCurrency returns the currency associated to a name and may return errUnknownCurrency.
+func getCurrency(code string) (currency, error) {
+	c, ok := currencies[code]
+	if !ok {
+		return currency{}, errUnknownCurrency
+	}
+	return c, nil
+}
---
+++ chapter-05/final/money/currency_internal_test.go
@@ -0,0 +1,29 @@
+package money
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestGetCurrency_EUR(t *testing.T) {
+	expected := currency{
+		code:      "EUR",
+		precision: 2,
+	}
+
+	got, err := getCurrency("EUR")
+	if err != nil {
+		t.Errorf("expected no error, got %s", err.Error())
+	}
+
+	if got != expected {
+		t.Errorf("expected %q, got %q", expected, got)
+	}
+}
+
+func TestGetCurrency_UnknownCurrency(t *testing.T) {
+	_, err := getCurrency("UNKNOWN")
+	if !errors.Is(err, errUnknownCurrency) {
+		t.Errorf("expected error %s, got %v", errUnknownCurrency, err)
+	}
+}
