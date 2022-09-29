8959e408e0229aa4c64c99a774f5be73f58dbe30
Donia Chaiehloudj <donia.cld@gmail.com>
2022-09-29 13:36:52 +0200

    ch05: Add currency type

---
+++ chapter-05/final/money/currency.go
@@ -0,0 +1,7 @@
+package money
+
+// currency defines the code of a money and its precision.
+type currency struct {
+	code      string
+	precision int
+}
--- chapter-05/final/money/money.go
+++ chapter-05/final/money/money.go
@@ -3,20 +3,26 @@ package money
 import "fmt"
 
 // Convert converts an amount in the from currency into the to currency with the targetPrecision number of digits.
-func Convert(amount, from, to string, targetPrecision int) (string, error) {
+func Convert(amount, from, to string) (string, error) {
 	// parse
 	n, err := parseNumber(amount)
 	if err != nil {
 		return "", fmt.Errorf("unable to parse amount: %w", err)
 	}
 
+	c := currency{
+		code:      to,
+		precision: 2,
+	}
+
 	// get the change rate
-	r, err := fetchChangeRate(from, to)
+	r, err := fetchChangeRate(from, c.code)
 	if err != nil {
 		return "", fmt.Errorf("%w: %s", errUnknownChangeRate, err.Error())
 	}
+
 	// convert
-	convertedValue := n.applyChangeRate(r, targetPrecision)
+	convertedValue := n.applyChangeRate(r, c.precision)
 
 	// format
 	return convertedValue.String(), nil
