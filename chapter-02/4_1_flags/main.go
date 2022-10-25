8a66e0b6366ab6a37365bbb61e30aa432051e3f7
Donia Chaiehloudj <donia.cld@gmail.com>
2022-10-25 13:09:58 +0200

    ch02: Add languages comments

--- chapter-02/4_1_flags/main.go
+++ chapter-02/4_1_flags/main.go
@@ -19,12 +19,12 @@ type language string
 
 // phrasebook holds greeting for each supported language
 var phrasebook = map[language]string{
-	"el": "Χαίρετε Κόσμε",
-	"en": "Hello world",
-	"fr": "Bonjour le monde",
-	"he": "שלום עולם",
-	"ur": "ہیلو دنیا",
-	"vi": "Xin chào Thế Giới",
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
 }
 
 // greet says hello to the world
