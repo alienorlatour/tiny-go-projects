83100c81f278e24d4144fc4589b3f51c8697fa5c
Aliénor Latour <alienor.latour@gmail.com>
2022-11-01 09:50:58 +0100

    Zealous replacement

--- chapter-03/exercises/7_alternative_exposition/pocketlog/logger.go
+++ chapter-03/exercises/7_alternative_exposition/pocketlog/logger.go
@@ -12,7 +12,7 @@ type Logger struct {
 	output    io.Writer
 }
 
-// New returns you a logger, ready to logf at the required threshold.
+// New returns you a logger, ready to log at the required threshold.
 // Give it a list of configuration functions to tune it at your will.
 // The default output is Stdout.
 func New(threshold Level, configFuncs ...ConfigFunc) *Logger {
