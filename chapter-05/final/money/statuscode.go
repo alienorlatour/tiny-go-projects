0650e763c7acc9f0ffd6bd29961195a2238fb529
Pascal Bertrand <floppyzedolfin@gmail.com>
2022-12-11 19:45:58 +0000

    read response from ecb

---
+++ chapter-05/final/money/statuscode.go
@@ -0,0 +1,14 @@
+package money
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func checkStatusCode(statusCode int) error {
+	// TODO: Handle 4xx and 5xx separately ?
+	if statusCode != http.StatusOK {
+		return fmt.Errorf("invalid status code %d", statusCode)
+	}
+	return nil
+}
