17c58689f4727e95f019e31c72647b486ec0b93e
Donia Chaiehloudj <donia.cld@gmail.com>
2023-01-05 11:53:33 +0100

    Add tests on Convert not working :cross:

--- chapter-05/layered/money/convert_test.go
+++ chapter-05/layered/money/convert_test.go
@@ -1,6 +1,8 @@
 package money_test
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/ablqk/tiny-go-projects/chapter-05/layered/money"
@@ -30,18 +32,48 @@ func TestConvert(t *testing.T) {
 				}
 			},
 		},
+		// TODO: Handle edge cases
 		"34.98 EUR to KRW": {
 			amount:          "34345982398459834.98",
 			from:            "EUR",
 			to:              "KRW",
 			targetPrecision: 2,
-			rateRepo:        stubRate{rate: 1.5},
+			rateRepo:        stubRate{rate: 1.2564},
 			validate: func(t *testing.T, got string, err error) {
 				if err != nil {
 					t.Errorf("expected no error, got %s", err.Error())
 				}
 				if got != "43.95" {
-					t.Errorf("expected 53.06, got %q", got)
+					t.Errorf("expected 51518973597689744.40, got %q", got)
+				}
+			},
+		},
+		// TODO: Handle edge cases
+		"0.001 EUR to KRW": {
+			amount:          "0.001",
+			from:            "EUR",
+			to:              "KRW",
+			targetPrecision: 2,
+			rateRepo:        stubRate{rate: 1.2564},
+			validate: func(t *testing.T, got string, err error) {
+				if err != nil {
+					t.Errorf("expected no error, got %s", err.Error())
+				}
+				if got != "0.00" {
+					t.Errorf("expected 0.00, got %q", got)
+				}
+			},
+		},
+		// TODO Fix the test
+		"Unknown currency": {
+			amount:          "0.001",
+			from:            "EUR",
+			to:              "KRW",
+			targetPrecision: 2,
+			rateRepo:        stubRate{err: fmt.Errorf("unknown currency")},
+			validate: func(t *testing.T, got string, err error) {
+				if !errors.Is(err, fmt.Errorf("unknown currency")) {
+					t.Errorf("expected \"no change rate known between currencies: unable to get exchange rates: unknown currency\" error, got %s", err.Error())
 				}
 			},
 		},
