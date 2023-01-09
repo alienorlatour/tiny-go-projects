71dee643cbaea77c3e93b7ba83da3f2a37e751b0
Donia Chaiehloudj <donia.cld@gmail.com>
2023-01-09 12:13:00 +0100

    Add documentation to money and repository pkgs

---
+++ chapter-05/layered/repository/doc.go
@@ -0,0 +1,8 @@
+/*
+Package repository exposes an API to retrieve an exchange rate from a source to a target currency by calling an external API,
+here the European Central Bank (ecb).
+For example from USD to EUR, it will give the current exchange rate for the day 1.55.
+
+The host is part of the main in the configuration, and you can change the path to access it within the package.
+*/
+package repository
