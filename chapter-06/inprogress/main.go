1672da3a5e19b34cad045a46451047c5b437799c
Pascal Bertrand <floppyzedolfin@gmail.com>
2023-01-23 08:42:44 +0000

    findMatchingBooks: code

--- chapter-06/inprogress/main.go
+++ chapter-06/inprogress/main.go
@@ -8,16 +8,18 @@ import (
 )
 
 func main() {
-	readers, err := loadData("testdata/readers.json")
+	readers, err := loadReaders("testdata/readers.json")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(readers)
+	matchingBooks := findMatchingBooks(readers)
+
+	fmt.Println(matchingBooks)
 }
 
-// loadData reads the file and returns the list of readers, and their beloved books, found therein.
-func loadData(filePath string) ([]Reader, error) {
+// loadReaders reads the file and returns the list of readers, and their beloved books, found therein.
+func loadReaders(filePath string) ([]Reader, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
@@ -36,4 +38,27 @@ func loadData(filePath string) ([]Reader, error) {
 	}
 
 	return readers, nil
+}
+
+// findMatchingBooks returns books that are on more than one reader's shelf.
+func findMatchingBooks(readers []Reader) []Book {
+	booksOnShelves := make(map[Book]uint)
+
+	// Register all books on shelves.
+	for _, reader := range readers {
+		for _, book := range reader.Books {
+			booksOnShelves[book]++
+		}
+	}
+
+	matchingBooks := make([]Book, 0)
+
+	// Find books that were added to shelves more than once.
+	for book, count := range booksOnShelves {
+		if count > 1 {
+			matchingBooks = append(matchingBooks, book)
+		}
+	}
+
+	return matchingBooks
 }
