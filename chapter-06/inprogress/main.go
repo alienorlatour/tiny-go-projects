6ba6828a4ef87a1e59c4f09edad431093279d04d
Donia Chaiehloudj <donia.cld@gmail.com>
2023-02-03 12:59:14 +0100

    ch06: Add suggestions

--- chapter-06/inprogress/main.go
+++ chapter-06/inprogress/main.go
@@ -10,12 +10,25 @@ func main() {
 
 	matchingBooks := findMatchingBooks(bookworms)
 
-	displayMatchingBooks(matchingBooks)
+	fmt.Println("Here are the matching books:")
+	displayBooks(matchingBooks)
+
+	fmt.Println("")
+
+	suggestions := suggestOtherBooks(bookworms)
+	displaySuggestions(suggestions)
 }
 
-func displayMatchingBooks(books []Book) {
-	fmt.Println("Here are the matching books:")
+func displayBooks(books []Book) {
 	for _, book := range books {
 		fmt.Println(book.Title, "by", book.Authors)
+	}
+}
+
+func displaySuggestions(suggestionsForBookworms []Bookworm) {
+	for _, bookworm := range suggestionsForBookworms {
+		fmt.Printf("Here are the suggestions for %s:\n", bookworm.Name)
+		displayBooks(bookworm.Books)
+		fmt.Println("")
 	}
 }
