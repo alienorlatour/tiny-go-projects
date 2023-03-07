8108396b69e1064434c368b3467880b2cf5f0e12
Aliénor Latour <alienor.latour@gmail.com>
2023-03-07 09:44:50 +0100

    Developing while writing - part 1 (#44)
    * Developing while writing - part 1
    * Add Makefile, fix typos and add test
    * Add an equal method in test instead of reflect.
    
    ---------
    
    Co-authored-by: Donia Chaiehloudj <donia.cld@gmail.com>

---
+++ chapter-06/1_load/bookworm_internal_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"testing"
+)
+
+var (
+	handmaidsTale = Book{Author: "Margaret Atwood", Title: "The Handmaid's Tale"}
+	oryxAndCrake  = Book{Author: "Margaret Atwood", Title: "Oryx and Crake"}
+	theBellJar    = Book{Author: "Sylvia Plath", Title: "The Bell Jar"}
+	janeEyre      = Book{Author: "Charlotte Brontë", Title: "Jane Eyre"}
+)
+
+func TestLoadBookworms_Success(t *testing.T) {
+	tests := map[string]struct {
+		bookwormsFile string
+		want          []Bookworm
+		wantErr       bool
+	}{
+		"file exists": {
+			bookwormsFile: "testdata/bookworms.json",
+			want: []Bookworm{
+				{Name: "Fadi", Books: []Book{handmaidsTale, theBellJar}},
+				{Name: "Peggy", Books: []Book{oryxAndCrake, handmaidsTale, janeEyre}},
+			},
+			wantErr: false,
+		},
+		"file doesn't exist": {
+			bookwormsFile: "testdata/no_file_here.json",
+			want:          nil,
+			wantErr:       true,
+		},
+		"invalid JSON": {
+			bookwormsFile: "testdata/invalid.json",
+			want:          nil,
+			wantErr:       true,
+		},
+	}
+	for name, testCase := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := loadBookworms(testCase.bookwormsFile)
+			if err != nil && !testCase.wantErr {
+				t.Fatalf("expected an error %s, got an empty one", err.Error())
+			}
+
+			if err == nil && testCase.wantErr {
+				t.Fatalf("expected no error, got one %s", err.Error())
+			}
+
+			if !equalBookworms(got, testCase.want) {
+				t.Fatalf("different result: got %v, expected %v", got, testCase.want)
+			}
+		})
+	}
+}
+
+// equalBookworms is a helper to test the equity of two list of Bookworms.
+func equalBookworms(bookworms, target []Bookworm) bool {
+	if len(bookworms) != len(target) {
+		// Early exit!
+		return false
+	}
+
+	for i := range bookworms {
+		// Verify the name of the Bookworm.
+		if bookworms[i].Name != target[i].Name {
+			return false
+		}
+
+		// Verify the content of the collections of Books for each Bookworm.
+		for j := range bookworms[i].Books {
+			if bookworms[i].Books[j] != target[i].Books[j] {
+				return false
+			}
+		}
+	}
+
+	// Everything is equal!
+	return true
+}
