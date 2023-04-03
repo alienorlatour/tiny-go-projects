474c83ea5b9e7cf1ee663dace66df253dcb4c0ac
Donia Chaiehloudj <donia.cld@isovalent.com>
2023-04-03 11:23:34 +0200

    :cross: Add main test but failing (need sorting)
    Signed-off-by: Donia Chaiehloudj <donia.cld@isovalent.com>

--- chapter-07/final/main_internal_test.go
+++ chapter-07/final/main_internal_test.go
@@ -5,6 +5,8 @@ func Example_main() {
 	// Output:
 	// Here are the common books:
 	// - The Handmaid's Tale by Margaret Atwood
-
-	// TODO: Include patterns
+	// Here are the common patterns:
+	// Craft:       knit; Name:   Plaid Pocket Socks; Yardage: 450 yards
+	// Craft:       knit; Name:               Sparky; Yardage: 2620 yards
+	// Craft:     sewing; Name:        Rosalie Skirt; Yardage: 3 yards
 }
