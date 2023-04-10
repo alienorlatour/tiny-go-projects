2d7bb82dfd8322e3dc1de551926eb2c8225789cb
Pascal Bertrand <floppyzedolfin@gmail.com>
2023-04-10 18:53:35 +0100

    fix uts 04-1.4

--- 04-logger/1_4_testing/pocketlog/logger_test.go
+++ 04-logger/1_4_testing/pocketlog/logger_test.go
@@ -5,5 +5,6 @@ import "learngo-pockets/logger/pocketlog"
 func ExampleLogger_Debugf() {
 	debugLogger := pocketlog.New(pocketlog.LevelDebug)
 	debugLogger.Debugf("Hello, %s", "world")
-	// Output: Hello, world
+	// We don't have any output yet.
+	// Output:
 }
