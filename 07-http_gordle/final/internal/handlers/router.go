a3a2a06433baee2a0ca540df7c2c34cd847dfffe
Aliénor Latour <alienor.latour@gmail.com>
2023-05-04 09:08:24 +0200

    Using chi router

--- 07-http_gordle/final/internal/handlers/router.go
+++ 07-http_gordle/final/internal/handlers/router.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
-	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5"
 
 	"learngo-pockets/httpgordle/api"
+	"learngo-pockets/httpgordle/internal/handlers/getstatus"
+	"learngo-pockets/httpgordle/internal/handlers/guess"
+	"learngo-pockets/httpgordle/internal/handlers/newgame"
+	"learngo-pockets/httpgordle/internal/repository"
 )
 
 // NewRouter returns a router that listens for requests to the following endpoints:
@@ -12,12 +16,12 @@ import (
 //   - Make a guess in a game.
 //
 // The provided router is ready to serve.
-func NewRouter() chi.Router {
+func NewRouter(gr *repository.GameRepository) chi.Router {
 	r := chi.NewRouter()
 
-	r.Post(api.NewGamePath, newGameHandler) // curl -X POST -v http://localhost:9090/games
-	r.Get(api.GetStatusPath, getStatus)     // curl -X GET -v http://localhost:9090/games/1682279480
-	r.Put(api.GuessPath, guessHandler)      // curl -X PUT -v http://localhost:9090/games/1682279480 -d '{"value":"faune"}'
+	r.Post(api.NewGamePath, newgame.Handler(gr))    // curl -X POST -v http://localhost:9090/games
+	r.Get(api.GetStatusPath, getstatus.Handler(gr)) // curl -X GET -v http://localhost:9090/games/1682279480
+	r.Put(api.GuessPath, guess.Handler(gr))         // curl -X PUT -v http://localhost:9090/games/1682279480 -d '{"value":"faune"}'
 
 	return r
 }
