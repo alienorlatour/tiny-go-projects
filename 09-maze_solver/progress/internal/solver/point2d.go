1f7f2e0c242e8c7d5d08ee3fe921bcb726105283
ablqk <alienor.latour@gmail.com>
2023-08-30 13:44:10 +0200

    [9.2] Comment

--- 09-maze_solver/progress/internal/solver/point2d.go
+++ 09-maze_solver/progress/internal/solver/point2d.go
@@ -1,10 +1,13 @@
 package solver
 
+// point2d represents the position of one pixel.
 type point2d struct {
 	x int
 	y int
 }
 
+// neighbours returns an array of the 4 neighbours of a pixel.
+// Some of the returned positions may be outside the image.
 func (p point2d) neighbours() []point2d {
 	return []point2d{
 		{p.x, p.y + 1},
