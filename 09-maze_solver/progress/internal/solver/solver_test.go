bf84029930cbabee5cd2923035e12cfb110b0bc7
ablqk <alienor.latour@gmail.com>
2023-08-30 14:11:10 +0200

    [9.2] More tests on the entrance finder

--- 09-maze_solver/progress/internal/solver/solver_test.go
+++ 09-maze_solver/progress/internal/solver/solver_test.go
@@ -15,9 +15,13 @@ func TestSolver_findEntrance_success(t *testing.T) {
 			inputPath: "testdata/maze10_10.png",
 			want:      point2d{0, 5},
 		},
-		"somewhere else": {
-			inputPath: "testdata/maze15_15.png",
-			want:      point2d{0, 7},
+		"400 px": {
+			inputPath: "testdata/maze400_400.png",
+			want:      point2d{0, 200},
+		},
+		"treasure near entrance": {
+			inputPath: "testdata/maze10_exit.png",
+			want:      point2d{0, 5},
 		},
 	}
 	for name, tt := range tests {
@@ -37,6 +41,35 @@ func TestSolver_findEntrance_success(t *testing.T) {
 			got, err := s.findEntrance()
 
 			assert.Equalf(t, tt.want, got, "findEntrance()")
+		})
+	}
+}
+
+func TestSolver_findEntrance_error(t *testing.T) {
+	tests := map[string]struct {
+		inputPath string
+	}{
+		"entrance in a corner": {
+			inputPath: "testdata/maze10+corner.png",
+		},
+	}
+	for name, tt := range tests {
+		name, tt := name, tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			img, err := openImage(tt.inputPath)
+			require.NoError(t, err)
+
+			s := &Solver{
+				maze:   img,
+				config: defaultColours(),
+			}
+
+			_, err = s.findEntrance()
+
+			assert.Error(t, err)
 		})
 	}
 }
