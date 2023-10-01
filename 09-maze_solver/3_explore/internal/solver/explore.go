6bd4bc184d9d7d109ccd48f1ee4b10b7920c2ce6
Donia Chaiehloudj <donia.cld@gmail.com>
2023-10-01 10:57:26 +0200

    chap09: Add comments to `explore` function

--- 09-maze_solver/3_explore/internal/solver/explore.go
+++ 09-maze_solver/3_explore/internal/solver/explore.go
@@ -27,39 +27,49 @@ func (s *Solver) explore(pathToBranch []image.Point) {
 		return
 	}
 
+	// This is the current pixel we are at.
 	pos := pathToBranch[len(pathToBranch)-1]
+	// This is the pixel we come from.
 	previous := pathToBranch[len(pathToBranch)-2]
 
 	for s.solution == nil {
 		// We know we'll have between up to 3 new neighbours to explore.
 		candidates := make([]image.Point, 0, 3)
 		for _, n := range neighbours(pos) {
+			// We ignore the pixel we are coming from.
 			if n == previous {
 				continue
 			}
 
+			// We check the color of the neighbour to define its type.
 			switch s.maze.RGBAAt(n.X, n.Y) {
 			case s.config.treasureColour:
 				s.solution = append(pathToBranch, n)
 				slog.Info(fmt.Sprintf("Treasure found: %v!", s.solution))
 				return
 			case s.config.pathColour:
+				// This neighbour is a valid candidate to continue the exploration.
 				candidates = append(candidates, n)
 			}
 		}
 
 		switch len(candidates) {
 		case 0:
+			// This is a dead end, we just stop exploring.
 			slog.Info(fmt.Sprintf("I must have taken the wrong turn at %v.", pos))
 			return
 		case 1, 2, 3:
 			for i := 1; i < len(candidates); i++ {
 				branch := append(slices.Clone(pathToBranch), candidates[i])
+				// We publish the branch to explore.
 				s.pathsToExplore <- branch
 			}
 
+			// We continue exploring.
 			pathToBranch = append(pathToBranch, candidates[0])
+			// We update the current position as the previous.
 			previous = pos
+			// The new position is the first candidate of the list.
 			pos = candidates[0]
 
 		}
