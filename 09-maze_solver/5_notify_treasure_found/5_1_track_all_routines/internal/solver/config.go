505ec2ea98bac62cac87ecad11dd0c1c1492684a
Donia Chaiehloudj <donia.cld@isovalent.com>
2023-10-18 11:53:13 +0200

    [9] Harmonise color comments
    Signed-off-by: Donia Chaiehloudj <donia.cld@isovalent.com>

--- 09-maze_solver/5_notify_treasure_found/5_1_track_all_routines/internal/solver/config.go
+++ 09-maze_solver/5_notify_treasure_found/5_1_track_all_routines/internal/solver/config.go
@@ -14,9 +14,9 @@ type config struct {
 // defaultColours returns the colour configuration of our maze.
 func defaultColours() config {
 	return config{
-		wallColour:     color.RGBA{R: 0, G: 0, B: 0, A: 255},       // black
-		pathColour:     color.RGBA{R: 255, G: 255, B: 255, A: 255}, // white
-		entranceColour: color.RGBA{R: 0, G: 191, B: 255, A: 255},   // deep sky blue
+		wallColour:     color.RGBA{R: 0, G: 0, B: 0, A: 255},       // Black
+		pathColour:     color.RGBA{R: 255, G: 255, B: 255, A: 255}, // White
+		entranceColour: color.RGBA{R: 0, G: 191, B: 255, A: 255},   // Deep sky blue
 		treasureColour: color.RGBA{R: 255, G: 0, B: 128, A: 255},   // Pink
 		solutionColour: color.RGBA{R: 225, G: 140, B: 0, A: 255},   // Orange
 	}
