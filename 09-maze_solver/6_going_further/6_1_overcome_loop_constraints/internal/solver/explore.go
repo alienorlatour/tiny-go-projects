9e87ef2f6407ca49f5993fc8837e936c40575e1d
Donia Chaiehloudj <donia.cld@isovalent.com>
2023-10-18 11:12:59 +0200

    [9.6.1] Color explored pixels
    Signed-off-by: Donia Chaiehloudj <donia.cld@isovalent.com>

---
+++ 09-maze_solver/6_going_further/6_1_overcome_loop_constraints/internal/solver/explore.go
@@ -0,0 +1,107 @@
+package solver
+
+import (
+	"fmt"
+	"image"
+	"log/slog"
+	"sync"
+)
+
+// listenToBranches creates a new routine for each branch published in s.pathsToExplore.
+func (s *Solver) listenToBranches() {
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	for {
+		select {
+		// s.quit will never return a value, unless something writes in it (which we don't do)
+		// or it has been closed, which we do when we find the treasure.
+		case <-s.quit:
+			slog.Info("the treasure has been found, worker going to sleep")
+			return
+		case p := <-s.pathsToExplore:
+			wg.Add(1)
+			go func(p *path) {
+				defer wg.Done()
+
+				s.explore(p)
+			}(p)
+		}
+	}
+}
+
+// explore one path and publish to the s.pathsToExplore channel
+// any branch we discover that we don't take.
+func (s *Solver) explore(pathToBranch *path) {
+	if pathToBranch == nil {
+		// This is a safety net. It should be used, but when it's needed, at least it's there.
+		return
+	}
+
+	pos := pathToBranch.at
+
+	for {
+		// Paint the current pixel as explored.
+		s.maze.Set(pos.X, pos.Y, s.config.exploredColour)
+
+		// Let's first check whether we should quit.
+		select {
+		case <-s.quit:
+			return
+		default:
+			// Continue the exploration.
+		}
+
+		// We know we'll have up to 3 new neighbours to explore.
+		candidates := make([]image.Point, 0, 3)
+
+		for _, n := range neighbours(pos) {
+			if pathToBranch.isPreviousStep(n) {
+				// Let's not return to the previous position
+				continue
+			}
+			// Look at the colour of this pixel.
+			// RGBAAt returns a color.RGBA{} zero value if the pixel is outside the bounds of the image.
+			switch s.maze.RGBAAt(n.X, n.Y) {
+			case s.config.treasureColour:
+				s.mutex.Lock()
+				// Even though we're inside a loop, we are returning from the function here,
+				// which makes it safe to defer the call to Unlock.
+				defer s.mutex.Unlock()
+
+				if s.solution == nil {
+					s.solution = &path{previousSteps: pathToBranch, at: n}
+					slog.Info(fmt.Sprintf("Treasure found: %v!", s.solution.at))
+					// s.quit <- struct{}{}
+					close(s.quit)
+				}
+				return
+
+			case s.config.pathColour:
+				candidates = append(candidates, n)
+			}
+		}
+
+		if len(candidates) == 0 {
+			slog.Debug("I must have taken the wrong turn.", "position", pos)
+			return
+		}
+
+		for _, candidate := range candidates[1:] {
+			branch := &path{previousSteps: pathToBranch, at: candidate}
+			// We are sure we send to pathsToExplore only when the quit channel isn't closed.
+			// A goroutine might have found the treasure since the check at the start of the loop.
+			select {
+			// s.quit returns a zero value only when the channel was closed, here -- when the exploration should end.
+			case <-s.quit:
+				slog.Debug("I'm an unlucky branch, someone else found the treasure, I give up.", "position", pos)
+				return
+			case s.pathsToExplore <- branch:
+				// continue execution after the select block
+			}
+		}
+
+		pathToBranch = &path{previousSteps: pathToBranch, at: candidates[0]}
+		pos = candidates[0]
+	}
+}
---
+++ 09-maze_solver/6_going_further/6_1_overcome_loop_constraints/internal/solver/solver.go
@@ -0,0 +1,65 @@
+package solver
+
+import (
+	"fmt"
+	"image"
+	"log/slog"
+	"sync"
+)
+
+// Solver is capable of finding the path from the entrance to the treasure.
+// The maze has to be a RGBA image.
+type Solver struct {
+	maze   *image.RGBA
+	config config
+
+	pathsToExplore chan *path
+	quit           chan struct{}
+
+	solution *path
+	mutex    sync.Mutex
+}
+
+// New builds a Solver by taking the path to the PNG maze, encoded in RGBA.
+func New(imagePath string) (*Solver, error) {
+	img, err := openMaze(imagePath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open maze image: %w", err)
+	}
+
+	return &Solver{
+		maze:           img,
+		config:         defaultColours(),
+		pathsToExplore: make(chan *path, 1),
+		quit:           make(chan struct{}),
+	}, nil
+}
+
+// Solve finds the path from the entrance to the treasure.
+func (s *Solver) Solve() error {
+	entrance, err := s.findEntrance()
+	if err != nil {
+		return fmt.Errorf("unable to find entrance: %w", err)
+	}
+
+	slog.Info(fmt.Sprintf("starting at %v", entrance))
+
+	// Write once in pathsToExplore before starting listening on the channel.
+	s.pathsToExplore <- &path{previousSteps: nil, at: entrance}
+	s.listenToBranches()
+
+	return nil
+}
+
+// findEntrance returns the position of the maze entrance on the image.
+func (s *Solver) findEntrance() (image.Point, error) {
+	for row := 0; row < s.maze.Bounds().Dy(); row++ {
+		for col := 0; col < s.maze.Bounds().Dy(); col++ {
+			if s.maze.RGBAAt(col, row) == s.config.entranceColour {
+				return image.Point{X: col, Y: row}, nil
+			}
+		}
+	}
+
+	return image.Point{}, fmt.Errorf("entrance position not found")
+}
