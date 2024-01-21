7f8e1e56d2d8abad59499155731f8f4e2b18995d
Donia Chaiehloudj <donia.cld@gmail.com>
2024-01-21 20:48:32 +0100

    Update integration test

--- 10-habits/progress/integration/integration_test.go
+++ 10-habits/progress/integration/integration_test.go
@@ -2,11 +2,10 @@ package integration
 
 import (
 	"context"
-	"net"
-	"testing"
-
 	"learngo-pockets/habits/internal/repository"
 	"learngo-pockets/habits/internal/server"
+	"net"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -35,9 +34,9 @@ func TestIntegration(t *testing.T) {
 	require.NoError(t, err)
 
 	// add 2 habits
-	addHabit(t, habitsCli, nil, "walk in the forest")
+	idWalk := addHabit(t, habitsCli, nil, "walk in the forest")
 
-	addHabit(t, habitsCli, ptr(3), "read a few pages")
+	idRead := addHabit(t, habitsCli, ptr(3), "read a few pages")
 
 	addHabitWithError(t, habitsCli, 5, "  	  ", codes.InvalidArgument)
 
@@ -51,6 +50,31 @@ func TestIntegration(t *testing.T) {
 			Name:            "read a few pages",
 			WeeklyFrequency: 3,
 		},
+	})
+
+	// add 2 ticks for Walk habit
+	tickHabit(t, habitsCli, idWalk)
+	tickHabit(t, habitsCli, idWalk)
+
+	// add 1 tick for Read habit
+	tickHabit(t, habitsCli, idRead)
+
+	getHabitStatusMatches(t, habitsCli, idWalk, &api.GetHabitStatusResponse{
+		Habit: &api.Habit{
+			Id:              idWalk,
+			Name:            "walk in the forest",
+			WeeklyFrequency: 1,
+		},
+		TicksCount: 2,
+	})
+
+	getHabitStatusMatches(t, habitsCli, idRead, &api.GetHabitStatusResponse{
+		Habit: &api.Habit{
+			Id:              idRead,
+			Name:            "read a few pages",
+			WeeklyFrequency: 3,
+		},
+		TicksCount: 1,
 	})
 }
 
@@ -77,6 +101,23 @@ func listHabitsMatches(t *testing.T, habitsCli api.HabitsClient, expected []*api
 	assert.ElementsMatch(t, list.Habits, expected)
 }
 
+func tickHabit(t *testing.T, habitsCli api.HabitsClient, id string) {
+	t.Helper()
+
+	_, err := habitsCli.TickHabit(context.Background(), &api.TickHabitRequest{
+		Id: id,
+	})
+	assert.NoError(t, err)
+}
+
+func getHabitStatusMatches(t *testing.T, habitsCli api.HabitsClient, id string, expected *api.GetHabitStatusResponse) {
+	h, err := habitsCli.GetHabitStatus(context.Background(), &api.GetHabitStatusRequest{Id: id})
+	require.NoError(t, err)
+
+	assert.Equal(t, expected.Habit, h.Habit)
+	assert.Equal(t, expected.TicksCount, h.TicksCount)
+}
+
 func newServer(t *testing.T) *grpc.Server {
 	t.Helper()
 	s := server.New(repository.New(), repository.NewTickRepository())
@@ -91,14 +132,16 @@ func ptr(i int32) *int32 {
 	return &i
 }
 
-func addHabit(t *testing.T, habitsCli api.HabitsClient, freq *int32, name string) {
+func addHabit(t *testing.T, habitsCli api.HabitsClient, freq *int32, name string) string {
 	t.Helper()
 
-	_, err := habitsCli.CreateHabit(context.Background(), &api.CreateHabitRequest{
+	resp, err := habitsCli.CreateHabit(context.Background(), &api.CreateHabitRequest{
 		Name:            name,
 		WeeklyFrequency: freq,
 	})
 	assert.NoError(t, err)
+
+	return resp.Habit.Id
 }
 
 func newClient(serverAddress string) (api.HabitsClient, error) {
