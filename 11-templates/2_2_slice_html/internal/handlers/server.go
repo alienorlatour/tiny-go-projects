1927b07c4f083d33da5f48dccfee9d4aa505ca4b
Aliénor Latour <alienor.latour@gmail.com>
2024-02-07 23:29:43 +0100

    Align the structure of the chapter with the code

--- 11-templates/2_2_slice_html/internal/handlers/server.go
+++ 11-templates/2_2_slice_html/internal/handlers/server.go
@@ -1,20 +1,37 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
+	"time"
+
+	habit "learngo-pockets/templates/internal/habits"
 
 	chi "github.com/go-chi/chi/v5"
 )
 
+// Client is the dependency towards the Habits service.
+//
+//go:generate minimock -i habitsClient -s "_mock.go" -o "mocks"
+type habitsClient interface {
+	ListHabits(ctx context.Context, t time.Time) ([]habit.Habit, error)
+}
+
+// Server serves all the HTML routes on this service.
 type Server struct {
+	client habitsClient
 	router chi.Router
 }
 
-func New() *Server {
-	return &Server{}
+// New builds a new server.
+func New(cli habitsClient) *Server {
+	return &Server{
+		client: cli,
+	}
 }
 
-func (s Server) Router() http.Handler {
+// Router returns an http handler that listens to all the proper paths.
+func (s *Server) Router() http.Handler {
 	r := chi.NewRouter()
 
 	r.Get("/", s.index)
--- 11-templates/7_funcmap/go.mod
+++ 11-templates/7_funcmap/go.mod
@@ -21,7 +21,8 @@ require (
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240125205218-1f4bbc51befe // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	google.golang.org/grpc v1.61.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
 
 replace learngo-pockets/habits => ../../10-habits/7_track
