abb870c586c86dac48d417bbce9662566d90f171
Pascal <pascal@blokur.com>
2024-02-13 09:14:23 +0000

    remove WeeklyFrequency type, replace it with TickCount, everywhere

--- 11-templates/2_3_list_habits/internal/habit/habit.go
+++ 11-templates/2_3_list_habits/internal/habit/habit.go
@@ -6,9 +6,6 @@ type ID string
 // Name is a short string that represents the name of a Habit.
 type Name string
 
-// WeeklyFrequency is the number of times a Habit should happen every week.
-type WeeklyFrequency uint
-
 // TickCount defines a number of weekly ticks.
 type TickCount uint
 
@@ -16,6 +13,6 @@ type TickCount uint
 type Habit struct {
 	ID              ID
 	Name            Name
-	WeeklyFrequency WeeklyFrequency
+	WeeklyFrequency TickCount
 	Ticks           TickCount
 }
