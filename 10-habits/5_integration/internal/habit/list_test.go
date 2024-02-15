094ea4f4ca5b9210b02d3dd11051ab02db8192ac
Donia Chaiehloudj <donia.cld@gmail.com>
2024-02-15 14:34:08 +0100

    Add a version of TestList without minimock

--- 10-habits/5_integration/internal/habit/list_test.go
+++ 10-habits/5_integration/internal/habit/list_test.go
@@ -83,3 +83,67 @@ func TestListHabits(t *testing.T) {
 		})
 	}
 }
+
+/******* Test List Habits without minimock *******/
+
+// MockList is a mock for FindAll method response.
+type MockList struct {
+	Items []habit.Habit
+	Err   error
+}
+
+// FindAll is a mock which returns the passed list of items and error.
+func (list MockList) FindAll(context.Context) ([]habit.Habit, error) { return list.Items, list.Err }
+
+// TestListHabitsWithoutMinimock uses a simple mock of the db.
+func TestListHabitsWithoutMinimock(t *testing.T) {
+	habits := []habit.Habit{
+		{
+			ID:              "123",
+			Name:            "walk",
+			WeeklyFrequency: 5,
+			CreationTime:    time.Now(),
+		},
+		{
+			ID:              "456",
+			Name:            "sleep",
+			WeeklyFrequency: 7,
+			CreationTime:    time.Now(),
+		},
+	}
+	dbErr := fmt.Errorf("db unavailable")
+
+	tests := map[string]struct {
+		db             MockList
+		expectedErr    error
+		expectedHabits []habit.Habit
+	}{
+		"empty": {
+			db:             MockList{Items: nil, Err: nil},
+			expectedErr:    nil,
+			expectedHabits: nil,
+		},
+		"2 items": {
+			db:             MockList{Items: habits, Err: nil},
+			expectedErr:    nil,
+			expectedHabits: habits,
+		},
+		"error case": {
+			db:             MockList{Items: nil, Err: dbErr},
+			expectedErr:    dbErr,
+			expectedHabits: nil,
+		},
+	}
+
+	for name, tc := range tests {
+		name, tc := name, tc
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := habit.ListHabits(context.Background(), tc.db)
+			assert.ErrorIs(t, err, tc.expectedErr)
+			assert.ElementsMatch(t, tc.expectedHabits, got)
+		})
+	}
+}
