77157b77ea0b8626d920f704cac32bfe8497faef
Aliénor Latour <alienor.latour@gmail.com>
2024-04-30 22:29:20 +0200

    Generating CSS

--- 11-templates/5_2_funcmap/internal/handlers/assets.go
+++ 11-templates/5_2_funcmap/internal/handlers/assets.go
@@ -1,28 +1,50 @@
 package handlers
 
 import (
+	_ "embed"
 	"net/http"
-	"slices"
+	"text/template"
 
 	"github.com/go-chi/chi/v5"
 )
 
-var supportedAssets = []string{
-	"styles.css",
-}
-
-// assets serves some identified static files. See the list above.
+// assets serves some identified files. See the list above.
 func (s *Server) assets(w http.ResponseWriter, r *http.Request) {
 	fileName := chi.URLParam(r, "filename")
 
+	// not really a good approach TODO
+	f, ok := map[string]http.HandlerFunc{
+		"styles.css": s.styles,
+	}[fileName]
+
 	// prevent injection
-	if !isValidAsset(fileName) {
+	if !ok {
 		http.Error(w, "file not found", http.StatusNotFound)
 	}
 
-	http.ServeFile(w, r, "internal/assets/"+fileName)
+	if f == nil {
+		http.ServeFile(w, r, "internal/handlers/assets/static/"+fileName)
+	}
+
+	s.lgr.Logf("generating file %s", fileName)
+	f(w, r)
 }
 
-func isValidAsset(name string) bool {
-	return slices.Contains(supportedAssets, name)
+//go:embed assets/dynamic/styles.css
+var stylesPage string
+
+func (s *Server) styles(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.New("styles").Parse(stylesPage)
+	if err != nil {
+		s.lgr.Logf("can't parse styles.css: %s", err.Error())
+		http.Error(w, "Error while rendering.", http.StatusInternalServerError)
+	}
+
+	w.Header().Add("Content-Type", "text/css")
+
+	err = tpl.Execute(w, "#023047")
+	if err != nil {
+		s.lgr.Logf("cannot render styles: %s", err.Error())
+		http.Error(w, "Error while rendering.", http.StatusInternalServerError)
+	}
 }
