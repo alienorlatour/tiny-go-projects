7b327b80bb50c4a8a539b3d345fde59f105200bf
Donia Chaiehloudj <donia.cld@gmail.com>
2024-05-04 13:55:38 +0200

    tmp commit

--- 12-wasm/factor_furry/cmd/wasm/main.go
+++ 12-wasm/factor_furry/cmd/wasm/main.go
@@ -19,19 +19,28 @@ func validate(this js.Value, args []js.Value) any {
 	expected := js.ValueOf(args[0]).Int()
 	answer := js.ValueOf(args[1]).Int()
 
-	// Multiplying operands and comparing with the answer provided by the user
+	var result bool
+
+	// Comparing with the answer provided by the user
 	if expected == answer {
 		fmt.Println("Correct!")
-		return "Correct!"
+		result = true
+
+		generate(this, args)
+	} else {
+		fmt.Println("Try again!")
 	}
-	fmt.Println("Try again!")
-	return "Try again!"
+
+	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", result)
+	return nil
 }
 
 func generate(this js.Value, args []js.Value) any {
 	operand1 := rand.Intn(10) + 1
 	operand2 := rand.Intn(10) + 1
 	expected := operand1 * operand2
+
+	fmt.Printf("%dx%d=%d\n", operand1, operand2, expected)
 
 	js.Global().Set("operand1", operand1)
 	js.Global().Set("operand2", operand2)
