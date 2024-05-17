85a5fcb3d52601da50749f984bf3bb87894bb5ec
Pascal <pascal@blokur.com>
2024-05-17 14:08:44 +0100

    read value and reset background

--- 12-wasm/factor_furry/cmd/wasm/main.go
+++ 12-wasm/factor_furry/cmd/wasm/main.go
@@ -23,43 +23,32 @@ func generate(this js.Value, args []js.Value) any {
 
 	js.Global().Get("document").Call("getElementById", "operand1").Set("innerHTML", operand1)
 	js.Global().Get("document").Call("getElementById", "operand2").Set("innerHTML", operand2)
-
-	js.Global().Set("operand1", operand1)
-	js.Global().Set("operand2", operand2)
+	js.Global().Get("document").Call("getElementById", "answer").Set("value", "")
 
 	return []interface{}{}
 }
 
 func validate(this js.Value, args []js.Value) any {
 	fmt.Println(args)
-	//op1 := js.ValueOf(args[0]).Int()
-	//fmt.Println(op1)
-	//op2 := js.ValueOf(args[1]).Int()
-	//fmt.Println(op2)
-	//value := js.ValueOf(args[2]).Int()
-	//fmt.Println(value)
 
-	operand1 := js.ValueOf(js.Global().Get("document").Call("getElementById", "operand1").Get("innerHTML"))
+	operand1 := js.Global().Get("document").Call("getElementById", "operand1").Get("innerHTML")
 	op1, err := strconv.Atoi(operand1.String())
 	if err != nil {
 		return fmt.Errorf("unknown format: %w", err)
 	}
 
-	fmt.Println(js.ValueOf(js.Global().Get("document").Call("getElementById", "operand1").Get("innerHTML")))
-
-	fmt.Println("operand2")
-	operand2 := js.ValueOf(js.Global().Get("document").Call("getElementById", "operand2").Get("innerHTML"))
+	operand2 := js.Global().Get("document").Call("getElementById", "operand2").Get("innerHTML")
 	op2, err := strconv.Atoi(operand2.String())
 	if err != nil {
 		return fmt.Errorf("unknown format: %w", err)
 	}
 
-	answer := js.ValueOf(js.Global().Get("document").Call("getElementById", "answer").Get("innerHTML"))
+	answer := js.Global().Get("document").Call("getElementById", "answer").Get("value")
 	a, err := strconv.Atoi(answer.String())
 	if err != nil {
 		return fmt.Errorf("unknown format: %w", err)
 	}
-	//op1 := js.ValueOf(args[0]).Int()
+
 	expected := op1 * op2
 
 	var result bool
