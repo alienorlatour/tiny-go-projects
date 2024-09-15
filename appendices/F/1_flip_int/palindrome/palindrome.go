553129f876d4723bc3c1e1d427d34264ee31ed0f
Donia Chaiehloudj <donia.cld@isovalent.com>
2024-09-15 20:09:41 +0200

    Rename in part 1 the bad var naming
    Signed-off-by: Donia Chaiehloudj <donia.cld@isovalent.com>

--- appendices/F/1_flip_int/palindrome/palindrome.go
+++ appendices/F/1_flip_int/palindrome/palindrome.go
@@ -8,18 +8,18 @@ import (
 // "1221" is a palindrome
 func IsPalindromeNumber(s string) bool {
 	// convert to a number
-	myInt, err := strconv.Atoi(s)
+	toInt, err := strconv.Atoi(s)
 	if err != nil {
 		return false
 	}
 
 	// flip the integer and compare
-	myIntOG := myInt
-	myInt2 := 0
-	for myInt > 0 {
-		myInt2 = 10*myInt2 + myInt%10 // 0*10 + 1234%10 = 4
-		myInt = myInt / 10            // 1234/10 = 123
+	original := toInt
+	flip := 0
+	for toInt > 0 {
+		flip = 10*flip + toInt%10 // 0*10 + 1234%10 = 4
+		toInt = toInt / 10        // 1234/10 = 123
 	}
 
-	return myIntOG == myInt2
+	return original == flip
 }
